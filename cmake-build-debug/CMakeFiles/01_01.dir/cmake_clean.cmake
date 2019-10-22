de660a97f322383e412a211a2a33cb2eda4ede9f
gianlucavinci98 <gianlucavinci98@live.it>
2019-10-22 10:46:29 +0200

    Finito

---
+++ cmake-build-debug/CMakeFiles/01_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_01"
+  "01_01.pdb"
+  "CMakeFiles/01_01.dir/01_01/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
