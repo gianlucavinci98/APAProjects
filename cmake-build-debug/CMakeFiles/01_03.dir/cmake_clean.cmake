31d30e9ce3b7143b8f361fc57e7aebf81cb78055
gianlucavinci98 <gianlucavinci98@live.it>
2019-10-23 13:04:20 +0200

    Completato acquisizione mat e richiesta menu - Modificare opportunatamente ruota() per funzionare con mat

---
+++ cmake-build-debug/CMakeFiles/01_03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_03"
+  "01_03.pdb"
+  "CMakeFiles/01_03.dir/01_03/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
