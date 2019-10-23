ebe4118ef497459dfaa56e178f06d408d66da1d9
gianlucavinci98 <gianlucavinci98@live.it>
2019-10-23 13:50:14 +0200

    Finito algoritmo Implementare migliori scelte

--- 01_03/main.c
+++ 01_03/main.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #define MAX 10
 
-void ruota(int v[], int p, int dir)
+void ruota(int v[], int p, int dir, int dim)
 {
-    int tmp[MAX];
+    int tmp[dim];
 
-    if (p>MAX) p = MAX%p;
-    if (dir == -1) p = MAX-p;
+    if (p>dim) p = p%dim;
+    if (dir == -1) p = dim-p;
 
     int c = 0;
-    for(int i=MAX-1; i>=0; i--)
+    for(int i=dim-1; i>=0; i--)
     {
-        if (i >= MAX-p)
+        if (i >= dim-p)
         {
             tmp[c] = v[i];
             c++;
@@ -44,6 +44,18 @@ void stampaVet(int vet[], int d)
         printf("%d ", vet[i]);
     }
 }
+void linearizza(int mat[][MAX], int vet[], int ind, int dim)
+{
+    for (int i = 0; i < dim; i++) {
+        vet[i]=mat[i][ind];
+    }
+}
+void normalizza(int vet[], int mat[][MAX], int ind, int dim)
+{
+    for (int i = 0; i < dim; i++) {
+        mat[i][ind]=vet[i];
+    }
+}
 
 int main()
 {
@@ -63,12 +75,28 @@ int main()
 
     stampaMat(mat, nr, nc);
 
-    printf("selettore | indice | direzione | posizioni: ");
-    scanf("%d %d %d %d", &sel, &ind, &dir, &pos);
+    printf("selettore: ");
+    scanf("%d", &sel);
     while(sel!=-1)
     {
-        printf("selettore | indice | direzione | posizioni: ");
-        scanf("%d %d %d %d", &sel, &ind, &dir, &pos);
+        printf("indice | direzione | posizioni: ");
+        scanf("%d %d %d", &ind, &dir, &pos);
+        if (sel==0)
+        {
+            ruota(mat[ind], pos, dir, nc);
+        }
+        else
+        {
+            int tmp[MAX];
+            linearizza(mat, tmp, ind, nr);
+            ruota(tmp, pos, dir, nr);
+            normalizza(tmp, mat, ind, nr);
+        }
+
+        stampaMat(mat, nr, nc);
+
+        printf("selettore: ");
+        scanf("%d", &sel);
     }
 
     return 0;
