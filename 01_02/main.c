93584f1a471b79ec2b4ffc6af0881670540172c6
gianlucavinci98 <gianlucavinci98@live.it>
2019-10-28 10:40:34 +0100

    Aggiunti commenti per verifica lab

--- 01_02/main.c
+++ 01_02/main.c
@@ -29,24 +29,24 @@ int conforntaParola(char str[], int pos, Entry diz[], int nd)
     int trovato = 0;
     int j=0, c, ok;
 
-    while (trovato==0 && j<nd)
+    while (trovato==0 && j<nd) //scorri per tutte il dizionario o finche è stato trovato match
     {
         c=0;
         ok=1;
-        while (ok==1 && c<strlen(diz[j].word))
+        while (ok==1 && c<strlen(diz[j].word)) //verifica corrispondenza lettera per lettera della parola del dizionario
         {
-            if (str[pos+c] != diz[j].word[c])
+            if (str[pos+c] != diz[j].word[c]) //se termina corrispondenza annulla flag e passa a prossima parola
             {
                 ok=0;
             }
             c++;
         }
-        if(ok==1) trovato=1;
+        if(ok==1) trovato=1; // se si arriva qui tutte le lettere corrispondono con una del dizionario
         j++;
     }
 
     if (trovato==0) return -1;
-    else return j-1;
+    else return j-1; //ritorna l'indice della parola nel dizionario
 }
 void processaRiga(char *str, Entry *diz, int nd)
 {
@@ -56,18 +56,18 @@ void processaRiga(char *str, Entry *diz, int nd)
     FILE *fp;
     fp = fopen("compresso.txt", "a");
 
-    while (i<strlen(str))
+    while (i<strlen(str)) //eseguo ciclo per ogni lettera della riga
     {
-        match = conforntaParola(str, i, diz, nd);
-        if (match==-1){
+        match = conforntaParola(str, i, diz, nd); //verica per ogni lettera se parte una corrispondenza
+        if (match==-1){ //se non c'è corrispondenza stampa semplicemente la lettera
             fprintf(fp, "%c", str[i]);
             printf("%c", str[i]);
         }
-        else
+        else //se c'è stampa il token corrispondente
         {
             fprintf(fp, "%s", diz[match].token);
             printf("%s", diz[match].token);
-            i = (int) (i + strlen(diz[match].word) - 1);
+            i = (int) (i + strlen(diz[match].word) - 1); //e sposta l'indice di lettera per il numero di lettere della parola trovata,per consentire di continuare ad analizzare la parola
         }
         i++;
     }
@@ -77,7 +77,7 @@ void processaRiga(char *str, Entry *diz, int nd)
 int main()
 {
     Entry diz[MAXDIZ];
-    int nd = caricaDizionario(diz);
+    int nd = caricaDizionario(diz); // carica il dizionario e restituisce il numero di coppie
     char riga[80];
 
     FILE *in;
--- 0_Prove/main.c
+++ 0_Prove/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    char riga[] = "IO\n ciao\ncacca";
+    int vet[10] =  {3,6,8,1,2,4,7,9,5,0};
+
+    for (int k = 0; k < 10 ; k++) {
+        printf("%d ",vet[k]);
+    }
+    printf("\n");
+
+    for (int i = 1; i < 10; i++) {
+        int tmp = vet[i];
+        int j = i-1;
+        while (tmp<vet[j] && j>=0)
+        {
+            vet[j+1]=vet[j];
+            j--;
+        }
+        vet[j+1]=tmp;
+
+    }
+
+    for (int k = 0; k < 10 ; k++) {
+        printf("%d ",vet[k]);
+    }
+    printf("\n");
 
     return 0;
 }
